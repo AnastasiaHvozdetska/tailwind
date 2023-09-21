34a45b2d4c73d489429b3a32296c8ad43ef82d7d
Anastasia Hvozdetska <anastasia9297@gmail.com>
2023-09-21 11:28:17 +0300

    Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,19 @@ export default {
         inter: "InterRegular",
         interBold: "InterBold",
         gilroy: "GilroyMedium",
+        gilroyBold: "GilroyBold",
         poppins: "PoppinsMedium",
+        gilroyMedium: "GilroyMedium",
       },
       fontSize: {
         "5xl": "85px",
         lg: ["20px", "30px"],
         lg2: ["20px", "25px"],
+        lg3: ["22px", "normal"],
+      },
+      lineHeight: {
+        "22px": "22px",
+        "30px": "30px",
       },
       colors: {
         purple: "#AE67FA",
@@ -20,11 +27,21 @@ export default {
         main: "#3E4581",
         dark: "#293134",
         linesColor: "rgba(185, 172, 209, 0.5)",
+        selago: "#EADFFB",
+        titanWhite: "#F8F8FF",
+        purpleHeart: "#7D30DF",
+        lightTitanWhite: "#ECEEFF",
       },
       spacing: {
         "115px": "115px",
         "190px": "190px",
         "30px": "30px",
+      },
+      maxWidth: {
+        700: "700px",
+      },
+      borderRadius: {
+        10: "10px",
       },
       animation: {
         rainbow: "rainbow 2s ease-in-out infinite",
@@ -32,7 +49,15 @@ export default {
       boxShadow: {
         "3xl": "0px 56px 63px 0px rgba(0, 0, 0, 0.22)",
       },
-
+      gridTemplateColumns: {
+        282: "repeat(3, 282px)",
+      },
+      skew: {
+        20: "20deg",
+      },
+      rotate: {
+        20: "20deg",
+      },
       keyframes: {
         rainbow: {
           "0%": { "background-position": "left" },
